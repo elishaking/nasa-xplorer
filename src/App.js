b4b0fd2241faa98a51b6d7bf08980b0c4217e747
King Elisha <ek.chibueze@gmail.com>
2020-01-07 23:10:35 +0100

    main markup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      <header>
-        <nav>
-          <h1>Nasa<span>Xplorer</span></h1>
-        </nav>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    searchTerm: ''
+  };
 
-export default App;
+  onSubmit = () => {
+
+  };
+
+  render() {
+    const { searchTerm } = this.state;
+
+    return (
+      <div className="app">
+        <header>
+          <nav>
+            <h1>Nasa<span>Xplorer</span></h1>
+          </nav>
+        </header>
+
+        <div className="content">
+          <h1 className="headline">Xplore the wonders of the Universe</h1>
+
+          <form onSubmit={this.onSubmit}>
+            <input
+              name="searchTerm"
+              placeholder="e.g. moon, mars, jupiter, etc"
+              value={searchTerm} />
+
+            <div className="actions">
+              <input type="submit" value="Search" />
+              <input type="button" value="Today" />
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
